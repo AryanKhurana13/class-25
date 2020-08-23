d306035bf343861b66da5c60036fdf4760e853c4
AryanKhurana13 <69413724+AryanKhurana13@users.noreply.github.com>
2020-08-23 14:54:41 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,9 @@
+class Box extends BaseClass{
+  constructor(x, y, width, height) {
+    super(x, y, width, height);
+    this.image = loadImage("sprites/wood1.png");
+    
+  }
+ };
+
+//super() - transfers all the properties from the constructor of the Base Class
